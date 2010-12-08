95b4259cddec79993e4181d3806d46c56ca08d25
ritikabose <r3majumdar@yahoo.com>
2010-12-08 13:07:31 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sonu::Application.config.secret_token = '0e5fe984f361ec21bd34961e9258a29c4ee8c5cc9fd1d7b936846a35597c614a8db9639439fed05648c0f3cbc34740fd3916d4322ba9dfe29cbff48788059579'
